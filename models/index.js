9c087e711dfdc165040b5112926d388a0b6c8d91
Peyton Schlafley <pschlafley0@gmail.com>
2020-11-11 19:24:10 -0500

    created like model and associations to the user, and post models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
+const Like = require('./Like');
 
 User.hasMany(Post, {
     foreignKey: 'user_id' 
@@ -26,4 +27,30 @@ Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Comment };
+Like.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Like.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.belongsToMany(Post, {
+    through: Like,
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Like,
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Like, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Like, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post, Comment, Like };
