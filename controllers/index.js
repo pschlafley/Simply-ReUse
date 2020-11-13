e57a678f76f47b52fead865700e94da6ce7bc710
Spiewak <bspiewak6@gmail.com>
2020-11-13 13:26:23 -0500

    created main and homepage handlebars files along with home-routes to render page

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-// const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes');
 
 // const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 
-// router.use('/', homeRoutes)
+router.use('/', homeRoutes);
 
 // router.use('/dashboard', dashboardRoutes);
 
--- server.js
+++ server.js
@@ -21,19 +21,19 @@ require('dotenv').config();
 //     })
 // }
 
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({ helpers });
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
