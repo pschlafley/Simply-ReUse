dd1611ec4045981e34e12adedc72a883ed96f1ea
Peyton Schlafley <pschlafley0@gmail.com>
2020-11-12 20:39:30 -0500

    implemented bcrypt to hash user's passwords

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const { Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
-
-class User extends Model {}
-
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 User.init (
     {
         id: {
@@ -33,12 +35,21 @@ User.init (
         }
     },
     {
+        hooks: {
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'user'
-    }
-);
-
+      }
+    );
 module.exports = User;
