912b3fb54413a41859c25c56e7b32c337e8915ff
lucianne302 <lucianne21@comcast.net>
2020-11-14 00:10:35 -0500

    added auth

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Comment.findAll({
@@ -46,7 +47,7 @@ router.get('/:id', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Comment.create({
       comment_text: req.body.comment_text,
       user_id: req.body.user_id,
@@ -60,7 +61,7 @@ router.post('/', (req, res) => {
   });
 
 // update a comment
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Comment.update(req.body,
     {
       where: {
@@ -82,7 +83,7 @@ router.put('/:id', (req, res) => {
 });
 
 // delete a comment
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Comment.destroy({
       where: {
         id: req.params.id
