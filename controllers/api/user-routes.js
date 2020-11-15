ca993bb4adcc3d152770c47949b706c38b70ada8
Spiewak <bspiewak6@gmail.com>
2020-11-15 17:52:53 -0500

    fixed user-routes.js and index.js associations bug

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-    User.findAll({})
+    User.findAll({
+        attributes: { exclude: ['password']}
+    })
     .then(dbUserData => {
         if(!dbUserData){
             res.status(404).json({ message: 'No users found' });
@@ -16,24 +18,41 @@ router.get('/', (req, res) => {
     })
 });
 
+// GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        where: {
-            id: req.params.id
-        }
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'content', 'created_at']
+        },
+        // included the Comment model here:
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+      ]
     })
-    .then(dbUserData => {
-        if(!dbUserData){
-            res.status(404).json({ message: 'No users found' });
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
             return;
-        }
-        res.json(dbUserData)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
 
 router.post('/', (req, res) => {
     User.create({
--- models/index.js
+++ models/index.js
@@ -4,53 +4,60 @@ const Post = require('./Post');
 const Like = require('./Like');
 
 User.hasMany(Post, {
-    foreignKey: 'user_id' 
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
-Like.belongsTo(User, {
-    foreignKey: 'user_id'
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
-Like.belongsTo(Post, {
-    foreignKey: 'post_id'
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
-User.belongsToMany(Post, {
-    through: Like,
-    foreignKey: 'user_id'
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
-Post.belongsToMany(User, {
-    through: Like,
-    foreignKey: 'post_id'
-});
 
-Post.hasMany(Like, {
-    foreignKey: 'post_id'
-});
+// Like.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
-User.hasMany(Like, {
-    foreignKey: 'user_id'
-});
+// Like.belongsTo(Post, {
+//     foreignKey: 'post_id'
+// });
+
+// User.belongsToMany(Post, {
+//     through: Like,
+//     foreignKey: 'user_id'
+// });
+
+// Post.belongsToMany(User, {
+//     through: Like,
+//     foreignKey: 'post_id'
+// });
+
+// Post.hasMany(Like, {
+//     foreignKey: 'post_id'
+// });
+
+// User.hasMany(Like, {
+//     foreignKey: 'user_id'
+// });
 
 module.exports = { User, Post, Comment, Like };
