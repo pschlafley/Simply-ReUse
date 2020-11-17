812fa21537aaea0453272e9d519de4950d693fa4
lucianne302 <lucianne21@comcast.net>
2020-11-17 02:05:50 -0500

    created tests for URL and date format

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//     // change date to M/DD/YYYY format
-//     format_date: date => {
-//       return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-//         date
-//       ).getFullYear()}`;
-// },
+module.exports = {
+    // change date to M/DD/YYYY format
+    format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+},
 
 // // change comment to plural if there is more than 1 comment
 // format_plural: (word, amount) => {
@@ -12,19 +12,19 @@
 //       return `${word}s`;
 //     }
 //     return word;
-// },
+// }
 
-// // format url link with replace and split methods
-// format_url: url => {
-//     return url
-//       .replace('http://', '')
-// },
-// format_url: url => {
-//     return url
-//       .replace('http://', '')
-//       .replace('https://', '')
-//       .replace('www.', '')
-//       .split('/')[0]
-//       .split('?')[0];
-//   },
-// };
+// format url link with replace and split methods
+format_url: url => {
+    return url
+      .replace('http://', '')
+},
+format_url: url => {
+    return url
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
+  },
+};
