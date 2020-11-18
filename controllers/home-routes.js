4f9b2a1f4a02ba07707cd80c7fc4f0c39816401e
Spiewak <bspiewak6@gmail.com>
2020-11-18 10:36:25 -0500

    removed dashboard page, updated signup redirect and started to test seeding  articles into blog

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,111 +1,42 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
-// route to render homepage and place single post in blog section using req.session
+// route to render homepage and place single posts in blog section using req.session
 router.get('/', (req, res) => {
-  Post.findOne({
-    attributes: [
-      'id',
-      'title',
-      'content', 
-      'created_at'
-    ],
-    include: [
-    {
-      model: Comment,
-      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-      include: {
-        model: User,
-        attributes: ['username']
-      }
-    },
-    {
-      model: User,
-      attributes: ['username']
+  const post = {
+    id: 1,
+    title: 'Title of Test Blog Post',
+    content: 'Test content',
+    created_at: new Date(),
+    comments: [{}, {}],
+    user: {
+      username: 'User1'
     }
-  ]
-})
-.then(dbPostData => {
-  // if (!dbPostData) {
-  //   res.status(404).json({ message: 'No post found with this id' });
-  //   return;
-  // }
-
-  // serialize the data
-  //const post = dbPostData.get({ plain: true });
+  };
 
   res.render('homepage', { 
-    //post,
+    post,
     loggedIn: req.session.loggedIn
-    });
-  })
-  .catch(err => {
-  console.log(err);
-  res.status(500).json(err);
-  });
+   });
 });
-
-// router.get('/', (req, res) => {
-//   const post = {
-//     id: 1,
-//     title: 'Title of Test Blog Post',
-//     content: 'Test content',
-//     created_at: new Date(),
-//     comments: [{}, {}],
-//     user: {
-//       username: 'User1'
-//     }
-//   };
-
-//   res.render('homepage', { 
-//     post,
-//     loggedIn: req.session.loggedIn
-//    });
-// });
-
 
 // single post render on blog page
 router.get('/blog', (req, res) => {
-  Post.findOne({
-    attributes: [
-      'id',
-      'title',
-      'content', 
-      'created_at'
-    ],
-    include: [
-    {
-      model: Comment,
-      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-      include: {
-        model: User,
-        attributes: ['username']
-      }
-    },
-    {
-      model: User,
-      attributes: ['username']
+  const post = {
+    id: 1,
+    title: 'Title of Test Blog Post',
+    content: 'Test content',
+    created_at: new Date(),
+    comments: [{}, {}],
+    user: {
+      username: 'User1'
     }
-  ]
-})
-.then(dbPostData => {
-  if (!dbPostData) {
-    res.status(404).json({ message: 'No post found with this id' });
-    return;
-  }
-
-  // serialize the data
-  const post = dbPostData.get({ plain: true });
+  };
 
   res.render('blog', { 
     post,
     loggedIn: req.session.loggedIn
-    });
-  })
-  .catch(err => {
-  console.log(err);
-  res.status(500).json(err);
-  });
+   });
 });
 
 // route to render the login page
@@ -143,10 +74,10 @@ router.get('/recycle', (req, res) => {
 });
 
 // route to render dashboard page
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard', {
-    loggedIn: req.session.loggedIn
-  });
-});
+// router.get('/dashboard', (req, res) => {
+//   res.render('dashboard', {
+//     loggedIn: req.session.loggedIn
+//   });
+// });
 
 module.exports = router;
