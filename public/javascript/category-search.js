cfc6f30d2324d6e53d2e588972aa67c6acc8164b
Spiewak <bspiewak6@gmail.com>
2020-11-19 15:00:47 -0500

    styles added for recycle items search

--- public/javascript/category-search.js
+++ public/javascript/category-search.js
@@ -11,10 +11,13 @@ async function categorySearch(event) {
     if (response.ok) {
         response.json().then(data => {
             for (let i = 0; i < data.items.length; i++) {
+                // let title = '<h5 class="card-title">' + 'Recyclable Items' + '</h5>'
                 let item  = '<li>' + data.items[i].item_name + '</li>'
-                console.log(data.items[i]);
+                // console.log(data.items[i]);
                 // need to append the item to page in the list from recycle.handlebars
+                // $("#category-items").append(title);
                 $("#category-items").append(item);
+
             }
         });
     };    
